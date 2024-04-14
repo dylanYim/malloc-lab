b23e6abe06a2a4de17de540a85faac6d07694b32
dylanYim <dongkyun94@gmail.com>
2024-04-14 15:18:16 +0900

    implement implict-first-fit

--- mm.c
+++ mm.c
@@ -44,28 +44,166 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
-/* 
+/*Basic constants and macros*/
+#define WSIZE     4  /*워드 크기*/
+#define DSIZE     8  /*더블 워드 크기*/
+#define CHUNKSIZE (1<<12)  /*초기 가용 블럭과 힙 확장을 위한 기본 크기*/
+
+#define MAX(x, y) ((x) > (y) ? (x) : y)  /*x, y 중 큰 값*/
+
+/*Pack a size and allocated bit into a word*/
+#define PACK(size, alloc)  ((size) | (alloc))  /*블록의 크기와 할당여부 비트를 통합 -> 헤더와 풋터에 저장되는 값*/
+
+/* Read and wirte a word at adress p*/
+#define GET(p)      (*(unsigned int *)(p)) /*p가 참조하는 워드를 읽어서 리턴*/
+#define PUT(p, val) (*(unsigned int *)(p) = (val)) /*p가 가리키는 워드에 val을 저장*/
+
+/* Read the size and allocated fileds from address p*/
+#define GET_SIZE(p)   (GET(p) & ~0x7) /*헤더나 풋터의 size 값을 리턴*/
+#define GET_ALLOC(p)  (GET(p) & 0x1)  /*헤더나 풋터의 할당비트를 리턴*/
+
+/* Given block ptr bp, compute address of its header and footer */
+#define HDRP(bp)   ((char *)(bp) - WSIZE) /*bp 가 참조하는 블록의 헤더 리턴*/
+#define FTRP(bp)   ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)  /*bp 가 참조하는 블록의 풋터 리턴*/
+
+/* Given block ptr bp, compute address of next and previous blocks */
+#define NEXT_BLKP(bp)  ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))  /*bp가 참조하는 블록의 다음 블록 리턴*/
+#define PREV_BLKP(bp)  ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))  /*bp가 참조하는 블록의 이전 블록 리턴*/
+char *heap_listp;
+/*
  * mm_init - initialize the malloc package.
  */
-int mm_init(void)
-{
-    return 0;
+void *coalesce(void *bp){
+  size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+  size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+  size_t size = GET_SIZE(HDRP(bp));
+
+  if (prev_alloc && next_alloc) { /* Case 1*/
+    return bp;
+  } else if (prev_alloc && !next_alloc) {
+    size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
+  } else if (!prev_alloc && next_alloc) {
+    size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+    PUT(FTRP(bp), PACK(size, 0));
+    PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+    bp = PREV_BLKP(bp);
+  } else {
+    size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
+    PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+    PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+    bp = PREV_BLKP(bp);
+  }
+  return bp;
 }
 
+void *extend_heap(size_t words){
+  char *bp;
+  size_t size;
+
+  /*Allocate an even number of words to maintain alignment*/
+  size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
+  if ((long) (bp = mem_sbrk(size)) == -1) {
+    return NULL;
+  }
+
+  /*Initialize free block header/footer and the epilogue header*/
+  PUT(HDRP(bp), PACK(size, 0));
+  PUT(FTRP(bp), PACK(size, 0));
+  PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
+
+  /*Coalesce if the previous block was free*/
+  return coalesce(bp);
+}
+
+int mm_init(void)
+{
+  /* Create the initial empty heap*/
+  if ((heap_listp = mem_sbrk(4 * WSIZE)) == (void *) - 1) {
+    return -1;
+  }
+  PUT(heap_listp, 0);
+  PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1));
+  PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1));
+  PUT(heap_listp + (3*WSIZE), PACK(0, 1));
+  heap_listp += (2 * WSIZE);
+
+  /*Extend the empty heap with a free block of CHUNKSIZE bytes*/
+  if (extend_heap(CHUNKSIZE / WSIZE) == NULL) {
+    return -1;
+  }
+  return 0;
+}
+
+void *find_fit(size_t asize){/*First-fit search*/
+  void *bp;
+
+  for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
+    if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
+      return bp;
+    }
+  }
+  return NULL; /*NO fit*/
+}
+
+void place(void *bp, size_t asize){
+  size_t csize = GET_SIZE(HDRP(bp));
+
+  if ((csize - asize) >= (2 * DSIZE)) {
+    PUT(HDRP(bp), PACK(asize, 1));
+    PUT(FTRP(bp), PACK(asize, 1));
+    bp = NEXT_BLKP(bp);
+    PUT(HDRP(bp), PACK(csize - asize, 0));
+    PUT(FTRP(bp), PACK(csize - asize, 0));
+  } else {
+    PUT(HDRP(bp), PACK(csize, 1));
+    PUT(FTRP(bp), PACK(csize, 1));
+  }
+}
 /* 
  * mm_malloc - Allocate a block by incrementing the brk pointer.
  *     Always allocate a block whose size is a multiple of the alignment.
  */
 void *mm_malloc(size_t size)
 {
-    int newsize = ALIGN(size + SIZE_T_SIZE);
-    void *p = mem_sbrk(newsize);
-    if (p == (void *)-1)
-	return NULL;
-    else {
-        *(size_t *)p = size;
-        return (void *)((char *)p + SIZE_T_SIZE);
-    }
+//    int newsize = ALIGN(size + SIZE_T_SIZE);
+//    void *p = mem_sbrk(newsize);
+//    if (p == (void *)-1)
+//	return NULL;
+//    else {
+//        *(size_t *)p = size;
+//        return (void *)((char *)p + SIZE_T_SIZE);
+//    }
+  size_t asize;
+  size_t extendsize;
+  char* bp;
+
+  /* Ignore spurious requests*/
+  if (size == 0) {
+    return NULL;
+  }
+
+  /* Adjust block size to include overhead and alignment reqs*/
+  if (size <= DSIZE) {
+    asize = 2 * DSIZE;
+  } else {
+    asize = DSIZE * ((size + (DSIZE) + (DSIZE - 1)) / DSIZE);
+  }
+
+  /*Search the free list for a fit*/
+  if ((bp = find_fit(asize)) != NULL) {
+    place(bp, asize);
+    return bp;
+  }
+
+  /*No fit found. Get more memory and place the block*/
+  extendsize = MAX(asize, CHUNKSIZE);
+  if ((bp = extend_heap(extendsize / WSIZE)) == NULL) {
+    return NULL;
+  }
+  place(bp, asize);
+  return bp;
 }
 
 /*
@@ -73,6 +211,11 @@ void *mm_malloc(size_t size)
  */
 void mm_free(void *ptr)
 {
+  size_t size = GET_SIZE(HDRP(ptr));
+
+  PUT(HDRP(ptr), PACK(size, 0));
+  PUT(FTRP(ptr), PACK(size, 0));
+  coalesce(ptr);
 }
 
 /*
@@ -87,7 +230,7 @@ void *mm_realloc(void *ptr, size_t size)
     newptr = mm_malloc(size);
     if (newptr == NULL)
       return NULL;
-    copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
+    copySize = GET_SIZE(HDRP(oldptr));
     if (size < copySize)
       copySize = size;
     memcpy(newptr, oldptr, copySize);
